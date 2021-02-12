e45a90424f401baceda31e01608608b9a8fcb0af
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-12 13:46:52 +0000

    var turned to let

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('button').addEventListener('click', displayName)
 
 function displayName () {
-  var x = document.getElementById('name').value;
+  let x = document.getElementById('name').value
   alert('hello! ' + x)
 }
